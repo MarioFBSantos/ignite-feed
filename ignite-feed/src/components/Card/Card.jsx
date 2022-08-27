6997ac5381804ff05b104df9b0be72e14a1f07f3
MarioFBSantos <mariosantos3197@gmail.com>
2022-08-27 17:10:52 -0300

    global css

---
+++ ignite-feed/src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import styles from './Card.module.css';
+
+export function Card(){
+    const [mario] = [1,2,3,4,5,6,7]
+    return (
+        <div>
+            <div className={styles.card}>
+                aaaaaaa
+            </div>
+        </div>
+    )
+}
